e6edba9e4b3f2caabce1d8f804c50526ad6eb2f3
unknown <hator@jasiu.(none)>
2012-05-07 16:52:22 +0200

    added promo text on main site

--- views/main.view.php
+++ views/main.view.php
@@ -10,7 +10,20 @@
 	include_once('sidebar_logged.view.php');
 ?>		<section id="main">
 			<h2>Staszic Delta</h2>
-			<h3><- Przeglądaj materiały:</h3>
-			<p><a href="">Źródła projektu</a></p>
+			<h3><- Przeglądaj materiały</h3>
+			<p><a href="https://github.com/hator/staszic-delta">Źródła projektu</a></p>
+			<p><strong>Staszic delta</strong> to projekt platformy edukacyjnej, która zastępowałaby Dropboxa i Google Docs w przechowywaniu przez klasę ważnych materiałów lekcyjnych, opracowań, analiz i interpretacji, a także powtórzeń przed klasówką.</p>
+			<p>Lista obenych możliwości platfomy:</p>
+			<ul>
+				<li>dodawanie artykułów przez administratora, a wkrótce, przez wszystkich użytkowników</li>
+				<li>dostęp do platformy tylko dla zalgowanych - dokumenty pozostają w obiegu klasowym</li>
+			</ul>
+			<p>Lista osiągnięć technicznych:</p>
+			<ul>
+				<li>zmiana wyglądu strony (za pomocą żarówki nad menu) - biała skórka pozwala na łatwiejsze czytanie tekstu</li>
+				<li>menu w AJAX, dynamicznie rozwiające się</li>
+				<li>wykorzystanie wzorca MVC</li>
+				<li>bezpieczne formularze</li> 
+			</ul>
 		</section>
 <?php include_once('footer.view.php'); ?>
